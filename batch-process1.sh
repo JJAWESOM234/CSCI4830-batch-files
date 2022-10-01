354b2b4667148cb8cdbabc25bc33433d2c24a0f1
Jedadiah McFarland <jedmcfarland715@gmail.com>
2022-09-30 22:15:37 -0500

    Fix permission issues and simplify code

--- batch-process1.sh
+++ batch-process1.sh
@@ -4,9 +4,6 @@ for f in "$1"/*; do
     SIZE="$(du -sh "${f}" | cut -f1)"
     echo "Processing $f file..."
     echo "$SIZE"
-    # using wc command to count number of words
-	number_of_words=`wc --word < $file_path`
-	echo "Number of words: $number_of_words"
   fi
 done
 
